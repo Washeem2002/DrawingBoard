2a98b16ab22ff49fc9f972bbf385e8072ad71f24
Mwasheem007 <jkrowling7702@gmail.com>
2024-06-16 07:46:38 +0530

    eracer

--- src/reducers/erracer.js
+++ src/reducers/erracer.js
@@ -1,10 +1,10 @@
 const eracer=(e,ref)=>{
-  e.preventDefault();
+ 
     const canvas = ref.current;
     const ctx = canvas.getContext("2d");
     const rect = canvas.getBoundingClientRect();
-    const x1 = e.clientX - rect.left;
-    const y1 = e.clientY - rect.top;
+    const x1 = e.clientX 
+    const y1 = e.clientY 
       ctx.clearRect(x1-15,y1-15,30,30)
 }
 export default eracer;
