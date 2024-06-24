bcc043925783eeba3db28e2984dd2f18ca06cacb
Mwasheem007 <jkrowling7702@gmail.com>
2024-06-24 16:22:06 +0530

    hello there

--- src/app/componets/canvas.js
+++ src/app/componets/canvas.js
@@ -21,7 +21,9 @@ const Canvas=()=>{
     <>
       <div  className="overflow-hidden" onMouseDown={
         (e)=>{
-          e.preventDefault();
+          if(e.target.tagName !== 'INPUT')
+            {e.preventDefault();}
+          
           if(istouch.current)
             {
               return;
